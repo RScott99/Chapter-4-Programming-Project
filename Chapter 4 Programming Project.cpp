14f92650070ee0a3ceccbf94f0ceeb3c5a94694d
RScott99 <rsh06.rh@gmail.com>
2025-07-06 20:18:39 -0400

    added description of code's function

--- Chapter 4 Programming Project.cpp
+++ Chapter 4 Programming Project.cpp
@@ -1,4 +1,4 @@
-
+//This code is intended to take a month in number form (1-12), and a year to find how many days would be in said month during that year
 #include <iostream>
 using namespace std;
 int main()

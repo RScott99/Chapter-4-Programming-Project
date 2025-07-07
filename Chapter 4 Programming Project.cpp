016d891732e473f4bbb3acb28ef6ac7108a611ca
RScott99 <rsh06.rh@gmail.com>
2025-07-06 20:25:02 -0400

    added code description

--- Chapter 4 Programming Project.cpp
+++ Chapter 4 Programming Project.cpp
@@ -1,4 +1,6 @@
-
+//Should be able to use three geometrical formulas to answer questions that can be selected via
+//inputting the a number 1-4 (4 to quit), and then validate that there are no numbers outside of 1-4
+//and that there are no negative numbers for the circle's radius, rect. len./wid., or the triang's base/height.
 #include <iostream>
 using namespace std;
 int main()
